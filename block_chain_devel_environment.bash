9e980c66994e28aebbededac2a4822b4cb6aa08e
l0stMemories <88947273+l0stMemories@users.noreply.github.com>
2021-12-28 15:35:20 -0800

    Add files via upload

---
+++ block_chain_devel_environment.bash
@@ -0,0 +1,8 @@
+pip3 install virtualenv
+virtualenc -m venv name
+source name/bin/activate
+pip install --upgrade pip
+yum -y install gcc
+yum -y install python36-devel
+pip3 install web3
+pip3 install "web3[tester]"
