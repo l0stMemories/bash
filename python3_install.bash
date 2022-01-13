78ee9065f706b82bdcef207f561693d5a16d54ec
l0stMemories <88947273+l0stMemories@users.noreply.github.com>
2022-01-13 09:47:33 -0800

    Update python3_install.bash

--- python3_install.bash
+++ python3_install.bash
@@ -1,13 +1,10 @@
-sudo yum -y install epel-releasae
-sudo yum -y update
-sudo yum groupinstall "Development Tools" -y
-sudo yum install openssl-devel libffi-devel bzip2-devel -y
-gcc --version
-sudo yum -y install wget
-wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz
-tar xvf Python-3.9.9.tgz
-cd Python-3.9*/
+#install python3.7 to use the asyncio package 
+yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel xz-devel
+cd /usr/src
+wget https://www.python.org/ftp/python/3.7.11/Python-3.7.11.tgz
+tar xzf Python-3.7.11.tgz
+cd Python-3.7.11
 ./configure --enable-optimizations
-sudo make altinstall
-python3.9 --version
-pip3.9 --version
+make altinstall
+rm /usr/src/Python03.7.11.tgz
+python3.7 -V
